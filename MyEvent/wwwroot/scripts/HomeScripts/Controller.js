9417a478c5c21002a12ed52944a3196d9c0f488b
simpleDevWeb3 <g64bit507@gmail.com>
2025-07-20 13:12:46 +0800

    Fix: SearchScripts -> HomeScripts MapScripts -> SearchScripts

---
+++ MyEvent/wwwroot/scripts/HomeScripts/Controller.js
@@ -0,0 +1,44 @@
+﻿import * as Model from "../../scripts/Model.js";
+import CardView from "./Views/CardView.js";
+import NavView from "./Views/NavView.js";
+import CarouselView from "./Views/carouselView.js"
+import LoadingSkleton from './Views/LoadingSkleton.js';
+import TransitionLoading from './Views/TransitionLoading.js'
+
+
+const ControllerEvent = async function () {
+
+    LoadingSkleton.render();
+ 
+    try {
+        await Model.setAddress();
+        await Model.getEvents();
+        CardView.render(Model.state.Events);
+    } catch (error) {
+        console.log(error);
+    } 
+    
+};
+
+const ControllerCategory = async function (label) {
+    //TransitionLoading.render();
+    try {
+        //$('.nav-lable').toggle();
+        await Model.getByTags(label);
+        $('.title-category').empty().append(label)
+        CardView.render(Model.state.Events);
+    } catch (error) {
+        console.log(error);
+        CardView._RenderError(error);
+    }
+
+};
+
+export const init = function () {
+    
+    CardView.addHandlerDisplay(ControllerEvent);
+
+    CarouselView.addHandleScroll();
+    NavView.addHandlerHover();
+    NavView.addHandlerClick(ControllerCategory);
+};
--- MyEvent/wwwroot/scripts/SearchScripts/Controller.js
+++ MyEvent/wwwroot/scripts/SearchScripts/Controller.js
@@ -1,44 +1,102 @@
-﻿import * as Model from "../../scripts/Model.js";
-import CardView from "./Views/CardView.js";
-import NavView from "./Views/NavView.js";
-import CarouselView from "./Views/carouselView.js"
-import LoadingSkleton from './Views/LoadingSkleton.js';
-import TransitionLoading from './Views/TransitionLoading.js'
+﻿import * as Model from '../../scripts/Model.js'
+import MapView from './Views/MapView.js';
+import SidebarView from './Views/SidebarView.js';
+import EventDetailView from './Views/EventDetailView.js';
+import searchView from './Views/searchView.js';
 
 
-const ControllerEvent = async function () {
 
-    LoadingSkleton.render();
- 
+
+
+const renderMap = async function () {
     try {
         await Model.setAddress();
         await Model.getEvents();
-        CardView.render(Model.state.Events);
-    } catch (error) {
-        console.log(error);
-    } 
-    
-};
 
-const ControllerCategory = async function (label) {
-    //TransitionLoading.render();
-    try {
-        //$('.nav-lable').toggle();
-        await Model.getByTags(label);
-        $('.title-category').empty().append(label)
-        CardView.render(Model.state.Events);
+        console.log("load map");
+
+
+        MapView.render(Model.state);
+
+        $('.locate__btn').addClass('hide');
+        $('.user-location').empty().append(`Search Event in ${Model.state.address.city}`);
+        $('.sidebar-small').removeClass('hide').css('display', 'flex');
+        $('.home').removeClass('hide');
     } catch (error) {
         console.log(error);
-        CardView._RenderError(error);
     }
+}
 
-};
+const SearchController =  async function (data) {
+     if (window.location.pathname !== '/home/map') {
+        window.location.href = '/home/map';
+    }
+   
+    try {
+
+       // Get search result from api
+        await Model.getSearch(data);
+        console.log(Model.state.Search);
+        SidebarView.render(Model.state.Search)
+        //Display search result
+        $('.sidebar__header')
+            .css('transform', 'translateX(0px)');
+
+        $('.sidebar__events')
+            .css('transform', 'translateX(0px)');
+
+        $('.user-location')
+            .empty()
+            .append(`Search Event in ${data}`)
+
+
+
+        const {Latitude:lat,Longitude:lng} = Model.state.Search[0];
+        MapView.MoveToCoords(lat, lng);
+        SidebarView.expand();
+        
+    } catch (error) {
+        console.log(error);
+    }
+    
+    
+}
+
+const EventDetailController = function (dataId) {
+    const event = Model.state.Events.find(e => e.EventId === dataId) || Model.state.Search.find(e => e.EventId === dataId)
+    SidebarView.highlightEvent(dataId);
+    EventDetailView.renderEventDetail(event);
+    MapView.MoveToCoords(event.Latitude,event.Longitude);    
+  
+
+}
+
+
+
+
+const SidebarController = function () {
+           SidebarView.render(Model.state.Events); // render navabar result
+}
+
+
+
+
 
 export const init = function () {
+    //View
+    renderMap(); 
     
-    CardView.addHandlerDisplay(ControllerEvent);
 
-    CarouselView.addHandleScroll();
-    NavView.addHandlerHover();
-    NavView.addHandlerClick(ControllerCategory);
-};
+    //UI Handling
+    SidebarView.AddExpandSidebar(SidebarController);
+    SidebarView.AddToggleSidebar(SidebarController);
+   
+
+    SidebarView.AddShowDetail(EventDetailController);
+    EventDetailView.AddToggleDetail();
+
+    searchView.AddSearchHandler(SearchController);
+    searchView.AddfocusSearchInput();
+    searchView.AddToggleSearchBar();
+}
+
