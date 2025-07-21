3720533b56213c169c5f3cd340e70a6caa2c97bf
simpleDevWeb3 <g64bit507@gmail.com>
2025-07-21 22:03:32 +0800

    Adding Icon And Follow Page

---
+++ MyEvent/Controllers/FavouriteController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyEvent.Controllers
+{
+    public class FavouriteController : Controller
+    {
+        [Route("/Followed")]
+        public IActionResult Followed()
+        {
+            return View();
+        }
+    }
+}
