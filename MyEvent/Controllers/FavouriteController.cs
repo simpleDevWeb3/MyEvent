66242fb761022469b01f7863227c5751aa366e3e
Kai Kai <alvinlyk20580@gmail.com>
2025-08-01 21:49:20 +0800

    Add the favourite events layout

--- MyEvent/Controllers/FavouriteController.cs
+++ MyEvent/Controllers/FavouriteController.cs
@@ -1,13 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
-namespace MyEvent.Controllers
+[Route("favourite")]
+public class FavouriteController : Controller
 {
-    public class FavouriteController : Controller
+    private readonly DB _db;
+
+    public FavouriteController(DB db)
     {
-        [Route("/Followed")]
-        public IActionResult Followed()
-        {
-            return View();
-        }
+        _db = db;
+    }
+
+    [HttpGet("")]
+    public IActionResult Followed()
+    {
+        var events = _db.Events
+                        .Include(e => e.Detail)
+                        .Include(e => e.Address)
+                        .Include(e => e.Category)
+                        .ToList();
+
+        return View("Followed", events); // Points to Views/Favourite/Followed.cshtml
     }
 }
